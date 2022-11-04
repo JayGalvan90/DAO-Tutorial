93def41ace23555933e8b9f59886db5df8bd512e
JayGalvan90 <39308108+JayGalvan90@users.noreply.github.com>
2022-11-04 16:25:02 -0400

    DAO Wallet with NFT Holders.
    The DAO wallet lets NFT holders vote on proposals, that is connected to a fake NFT marketplace and has a 5 min window for Voting.

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x3eA909c688DEEc2C6238c6f886E4686C10E77d46";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
